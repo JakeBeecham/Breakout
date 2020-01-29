bc238ef1038277cd84e7918362df7ee9f80ace66
JakeBeecham <60218222+JakeBeecham@users.noreply.github.com>
2020-01-29 17:35:01 +0000

    Full Version
    Game Manager set up and game finished.

--- Breakout/Assets/Scripts/BallController.cs
+++ Breakout/Assets/Scripts/BallController.cs
@@ -33,9 +33,39 @@ public class BallController : MonoBehaviour
         PickRandomDirection(70, 350);//pick the angles we want the ball to move
     }
 
+    void Update()
+    {
+        //0 = left button
+        //1 = right button
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            //if the left mouse button was clicked down...
+            //convert from screen coordinate to world coordinate
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0;
+
+            //get the direction between the mouse and the ball
+            Vector3 newDirection = mousePosition - transform.position;
+            newDirection.Normalize();
+
+            //move the ball in the new direction
+            body.velocity = newDirection * movementSpeed;
+        }
+    }
+
     void ResetPosition()
     {
+        //reset the ball to where it was at the start of the game
         body.MovePosition(startPosition);
+
+        //pick a random scale
+        //scale between 25% and 100%
+        float randomScale = Random.Range(0.25f, 1);
+        Vector3 newScale = new Vector3(randomScale, randomScale, 1);
+
+        //apply that scale to the ball
+        transform.localScale = newScale;
     }
 
     void PickRandomDirection(float min, float max)
@@ -55,11 +85,13 @@ public class BallController : MonoBehaviour
         if (collision.gameObject.CompareTag("Block"))
         {
             //call the gamemanager to record the score
+            manager.OnBlockDestroyed();
             //destroy the block that was hit
             Destroy(collision.gameObject);
         }
         else if (collision.gameObject.CompareTag("Kill"))
         {
+            manager.OnBallHitKill();
             //restart the ball (reset position and pick and new angle)
             ResetPosition();
             PickRandomDirection(40, 130);
--- Breakout/Assets/Scripts/GameManager.cs
+++ Breakout/Assets/Scripts/GameManager.cs
@@ -1,17 +1,79 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    //how many blocks are left in the level
+    //when this is 0, the player has won
+    public int blocksRemaining;
 
-	void Start ()
+    //when the lives are 0, game over, player has lost
+    public int lives = 3;
+
+    //custom class to store data about the player
+    public PlayerData data;
+
+    //count how many blocks are in the level when the game starts
+    void CountBlocks()
     {
-		
-	}
-	
-	void Update ()
+        //GameObject.FindObjectWithTag
+        //GameObject.FindObjectWithTags
+        blocksRemaining = GameObject.FindGameObjectsWithTag("Block").Length;
+    }
+
+    //update the blocks remaining every time one is destroyed
+    public void OnBlockDestroyed()
     {
-		
-	}
+        //subtract 1 from blocks remaining
+        blocksRemaining--;
+
+        //update the player save data
+        data.totalBlocksDestroyed++;
+
+        if(blocksRemaining <= 0)
+        {
+            //game complete (win)!
+            //will load the scene with the matching name
+            SceneManager.LoadScene("GameWon");
+        }
+    }
+
+    //update the lives when the ball hits the kill barrier
+    public void OnBallHitKill()
+    {
+        //subtract 1 from lives
+        lives--;
+
+        //check if game over
+        if(lives <= 0)
+        {
+            //game over (loss)!
+            //will load the scene with the matching name
+            SceneManager.LoadScene("GameLost");
+        }
+    }
+
+    //Awake is called before the Start method
+    private void Awake()
+    {
+        //setup Unity to tell me when the level has loaded...
+        SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+    }
+
+    private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+        CountBlocks();
+    }
+}
+
+//lets the variables be edited in Unity
+[Serializable]
+public class PlayerData
+{
+    public int totalBlocksDestroyed;
+    public string userName = "Jake";
 }
