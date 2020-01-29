99b23bfe2288e00101b74ae66bd69a4280af8837
JakeBeecham <60218222+JakeBeecham@users.noreply.github.com>
2020-01-29 10:45:23 +0000

    created

---
+++ Breakout/Assets/Scripts/BallController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float movementSpeed = 10;
+    float currentSpeed;
+    Rigidbody2D body;
+    Vector2 direction;
+
+    //link the ball to the game manager
+    public GameManager manager;
+
+    //manager for playing sounds
+    AudioSource audioSource;
+
+    //what was the starting position of the ball?
+    Vector3 startPosition;
+
+    void Start()
+    {
+        //get the audio sourced attached to this game object
+        audioSource = GetComponent<AudioSource>();
+
+        body = GetComponent<Rigidbody2D>();
+        currentSpeed = movementSpeed;
+
+        //save the position at the start of the game in the variable startPosition
+        startPosition = transform.position;
+
+        ResetPosition();
+        PickRandomDirection(70, 350);//pick the angles we want the ball to move
+    }
+
+    void ResetPosition()
+    {
+        body.MovePosition(startPosition);
+    }
+
+    void PickRandomDirection(float min, float max)
+    {
+        direction.x = Random.Range(min, max);
+        direction.y = Random.Range(min, max);
+        direction.Normalize();
+
+        body.velocity = direction * movementSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        audioSource.Play();
+
+        //if the ball hits a block
+        if (collision.gameObject.CompareTag("Block"))
+        {
+            //call the gamemanager to record the score
+            //destroy the block that was hit
+            Destroy(collision.gameObject);
+        }
+        else if (collision.gameObject.CompareTag("Kill"))
+        {
+            //restart the ball (reset position and pick and new angle)
+            ResetPosition();
+            PickRandomDirection(40, 130);
+        }
+    }
+}
---
+++ Breakout/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Variables
+    public float movementSpeed = 10;//Speed we will move at
+    float horizontal;//store in the inout from the user
+    Rigidbody2D body;//rigid body to move the player
+    Vector3 nextPostion;//where we are going next
+
+
+    void Start ()
+    {
+        //get the rigid body from the game object
+        body = GetComponent<Rigidbody2D>();
+    }
+	
+	void Update ()
+    {
+        //use Input.GetAxis to get the Horizontal input (A,D, Left Arrow, Right Arrow)
+        //returns a value between -1 and 1 (-1 = moving left, 0 = not moving, 1 = moving right)
+        horizontal = Input.GetAxis("Horizontal");
+
+        //set our next position to be the direction we are moving * our speed * time pass since last update
+        //movementSpeed * Time.deltaTime (moving a percentage of the speed every frame)
+        nextPostion.x = horizontal * movementSpeed * Time.deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        //set the velocity of the object to move to our next position
+        body.velocity = nextPostion;
+    }
+}
